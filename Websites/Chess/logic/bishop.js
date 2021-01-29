5cb998ee8a2b2688a5fb3854ec2625fc9b711367
Ayush Sharma <ayushjunior@gmail.com>
2021-01-29 21:18:53 +0000

    Tried working on fixing the valid move spaces thing. Instead of making each class return every single move space possible, I filtered them in the class itself so that if a piece is blocked then it won't return moves that go over blocking piece (if a queen is blocked to the right then she shouldn't be able to jump over that piece to go to the right of the blocking piece). It is a headache. My code for calculating some of the valid move spaces could be improved and I will change it tomorrow. Right now when a Bishop looks at it's moves it will look from the farthest left move to the farther right. Which creates so many problems with filtering moves and same with Castle and Queen. So I will change them to use recursion and just look 1 move around the piece. It'll be much cleaner and the filerting will be 10000x easier since it would be the stopping statement for the recursion. For now, I wanna punch my screen so before that I will stop XD

--- Websites/Chess/logic/bishop.js
+++ Websites/Chess/logic/bishop.js
@@ -1,23 +1,54 @@
 class Bishop extends Piece {
   constructor(side, position) {
-    super(
-      side,
-      position,
-      "./images/" + ["white", "black"][side] + "/Bishop.JPG"
-    );
+    super(side, position, "./images/" + ["white", "black"][side] + "/Bishop.JPG");
   }
 
-  getMoves() {
+  getMoves(board) {
     let moves = [];
+    let spliceOffset = 0;
 
-    for (let i = -7; i < 8; i++) {
-      if (0 <= this._position[0] + i && this._position[0] + i <= 7) {
-        if (0 <= this._position[1] + i && this._position[1] + i <= 7) {
-          moves.push([this._position[0] + i, this._position[1] + i]);
+    for (let j = 0; j < 2; j++) {
+      spliceOffset = moves.length;
+      for (let i = -7; i < 8; i++) {
+        if (i === 0) {
+          continue;
         }
 
-        if (0 <= this._position[1] - i && this._position[1] - i <= 7)
-          moves.push([this._position[0] + i, this._position[1] - i]);
+        if (0 <= this._position[0] + i && this._position[0] + i <= 7) {
+          if (j === 0) {
+            if (0 <= this._position[1] + i && this._position[1] + i <= 7) {
+              moves.push([this._position[0] + i, this._position[1] + i]);
+
+              if (board[this._position[1] + i][this._position[0] + i][1] !== undefined) {
+                if (board[this._position[1] + i][this._position[0] + i][1].getSide() !== this._side) {
+                  moves.splice(spliceOffset, moves.length - spliceOffset - 1);
+                } else {
+                  moves.splice(spliceOffset, moves.length - spliceOffset);
+                }
+
+                if (i > 0) {
+                  break;
+                }
+              }
+            }
+          } else {
+            if (0 <= this._position[1] - i && this._position[1] - i <= 7) {
+              moves.push([this._position[0] + i, this._position[1] - i]);
+
+              if (board[this._position[1] - i][this._position[0] + i][1] !== undefined) {
+                if (board[this._position[1] - i][this._position[0] + i][1].getSide() !== this._side) {
+                  moves.splice(spliceOffset, moves.length - spliceOffset - 1);
+                } else {
+                  moves.splice(spliceOffset, moves.length - spliceOffset);
+                }
+
+                if (i > 0) {
+                  break;
+                }
+              }
+            }
+          }
+        }
       }
     }
 
--- Websites/Chess/logic/castle.js
+++ Websites/Chess/logic/castle.js
@@ -1,23 +1,49 @@
 class Castle extends Piece {
   constructor(side, position) {
-    super(
-      side,
-      position,
-      "./images/" + ["white", "black"][side] + "/Castle.JPG"
-    );
+    super(side, position, "./images/" + ["white", "black"][side] + "/Castle.JPG");
   }
 
-  getMoves() {
+  getMoves(board) {
     let moves = [];
+    let spliceOffset = 0;
 
-    for (let i = -7; i <= 7; i++) {
-      if (i !== 0) {
-        if (0 <= this._position[0] + i && this._position[0] + i <= 7) {
-          moves.push([this._position[0] + i, this._position[1]]);
-        }
+    for (let j = 0; j < 2; j++) {
+      spliceOffset = moves.length;
+      for (let i = -7; i <= 7; i++) {
+        if (i !== 0) {
+          if (j === 0) {
+            if (0 <= this._position[0] + i && this._position[0] + i <= 7) {
+              moves.push([this._position[0] + i, this._position[1]]);
 
-        if (0 <= this._position[1] + i && this._position[1] + i <= 7) {
-          moves.push([this._position[0], this._position[1] + i]);
+              if (board[this._position[1]][this._position[0] + i][1] !== undefined) {
+                if (board[this._position[1]][this._position[0] + i][1].getSide() !== this._side) {
+                  moves.splice(spliceOffset, moves.length - spliceOffset - 1);
+                } else {
+                  moves.splice(spliceOffset, moves.length - spliceOffset);
+                }
+
+                if (i > 0) {
+                  break;
+                }
+              }
+            }
+          } else {
+            if (0 <= this._position[1] + i && this._position[1] + i <= 7) {
+              moves.push([this._position[0], this._position[1] + i]);
+
+              if (board[this._position[1] + i][this._position[0]][1] !== undefined) {
+                if (board[this._position[1] + i][this._position[0]][1].getSide() !== this._side) {
+                  moves.splice(spliceOffset, moves.length - spliceOffset - 1);
+                } else {
+                  moves.splice(spliceOffset, moves.length - spliceOffset);
+                }
+
+                if (i > 0) {
+                  break;
+                }
+              }
+            }
+          }
         }
       }
     }
